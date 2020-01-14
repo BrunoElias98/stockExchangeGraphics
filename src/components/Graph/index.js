abc77f0d8898900fcfdd2b4ee0b5a0bb377a4029
BrunoElias98 <bruno.elias3312@gmail.com>
2020-01-13 23:54:00 -0300

    Alterando a forma como monta os Gráficos, atribuindo todos os gráficos a um array e feito um map para renderizar os mesmos

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -9,7 +9,7 @@ import api from '../../services/api'
 import { formatNumber } from '../../utils'
 
 export default function Graph(props) {
-
+    
     const url = props.location.pathname.split('/')
     const idCompany = url[2]
 
@@ -49,153 +49,152 @@ export default function Graph(props) {
 
         return 0
     }))
-
-    const optionsRevenue = {
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Receita'
-        },
-        series: [
-            {
-                name: 'Receita',
-                data: revenue.length !== 0 ?  revenue.map(revenue => ( [ revenue ] ))  : ''
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-    }
-
-    const optionsRevenueGrowth = {
-        chart: {
-            type: 'spline'
-        },
+    
+    const charts = [
+        revenue = {
+            chart: {
+                type: 'spline'
+            },
             title: {
-            text: 'Crescimento da Receita'
+                text: 'Receita'
+            },
+            series: [
+                {
+                    name: 'Receita',
+                    data: revenue.length !== 0 ?  revenue.map(revenue => ( [ revenue ] ))  : ''
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
         },
-        series: [
-            {
-                name: 'Crescimento da Receita',
-                data: revenueGrowth.length !== 0 ? revenueGrowth.map(revenueGrowth => ( [ revenueGrowth ] )) : ''
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
+    
+        revenueGrowth = {
+            chart: {
+                type: 'spline'
+            },
+                title: {
+                text: 'Crescimento da Receita'
+            },
+            series: [
+                {
+                    name: 'Crescimento da Receita',
+                    data: revenueGrowth.length !== 0 ? revenueGrowth.map(revenueGrowth => ( [ revenueGrowth ] )) : ''
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
         },
-    }
-
-    const optionsOperatingExpenses = {
-        chart: {
-            type: 'spline'
+    
+        operatingExpenses = {
+            chart: {
+                type: 'spline'
+            },
+                title: {
+                text: 'Despesas Operacionais'
+            },
+            series: [
+                {
+                    name: 'Despesas Operacionais',
+                    data: operatingExpenses.length !== 0 ? operatingExpenses.map(operatingExpenses => ( [ operatingExpenses ] )) : ''
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
         },
-            title: {
-            text: 'Despesas Operacionais'
+    
+        ebitdaMargin = {
+            chart: {
+                type: 'spline'
+            },
+                title: {
+                text: 'Margem EBTIDA'
+            },
+            series: [
+                {
+                    name: 'Margem EBTIDA',
+                    data: ebitdaMargin.length !== 0 ? ebitdaMargin.map(ebitdaMargin => ( [ ebitdaMargin ] )) : ''
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
         },
-        series: [
-            {
-                name: 'Despesas Operacionais',
-                data: operatingExpenses.length !== 0 ? operatingExpenses.map(operatingExpenses => ( [ operatingExpenses ] )) : ''
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
+    
+        ebitda = {
+            chart: {
+                type: 'spline'
+            },
+                title: {
+                text: 'EBTIDA'
+            },
+            series: [
+                {
+                    name: 'EBTIDA',
+                    data: ebitda.length !== 0 ? ebitda.map(ebitda => ( [ ebitda ] )) : '' 
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
         },
-    }
-
-    const optionsEbtidaMargin = {
-        chart: {
-            type: 'spline'
-        },
-            title: {
-            text: 'Margem EBTIDA'
-        },
-        series: [
-            {
-                name: 'Margem EBTIDA',
-                data: ebitdaMargin.length !== 0 ? ebitdaMargin.map(ebitdaMargin => ( [ ebitdaMargin ] )) : ''
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-    }
-
-    const optionsEbtida = {
-        chart: {
-            type: 'spline'
-        },
-            title: {
-            text: 'EBTIDA'
-        },
-        series: [
-            {
-                name: 'EBTIDA',
-                data: ebitda.length !== 0 ? ebitda.map(ebitda => ( [ ebitda ] )) : '' 
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-    }
-
-    const optionsConsolidatedIncome = {
-        chart: {
-            type: 'spline'
-        },
-            title: {
-            text: 'Renda Consolidada'
-        },
-        series: [
-            {
-                name: 'Renda Consolidada',
-                data: consolidatedIncome.length !== 0 ? consolidatedIncome.map(consolidatedIncome => ( [ consolidatedIncome ] )) : ''
-            }
-        ],
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-    }
+    
+        consolidatedIncome = {
+            chart: {
+                type: 'spline'
+            },
+                title: {
+                text: 'Renda Consolidada'
+            },
+            series: [
+                {
+                    name: 'Renda Consolidada',
+                    data: consolidatedIncome.length !== 0 ? consolidatedIncome.map(consolidatedIncome => ( [ consolidatedIncome ] )) : ''
+                }
+            ],
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+        }
+    ]   
 
     return (
         <>
-            <HighchartsReact highcharts={Highcharts} options={optionsRevenue} />
-            <HighchartsReact highcharts={Highcharts} options={optionsRevenueGrowth} />
-            <HighchartsReact highcharts={Highcharts} options={optionsOperatingExpenses} />
-            <HighchartsReact highcharts={Highcharts} options={optionsEbtidaMargin} />
-            <HighchartsReact highcharts={Highcharts} options={optionsEbtida} />
-            <HighchartsReact highcharts={Highcharts} options={optionsConsolidatedIncome} />
+            {charts.map((chart, index) => 
+                <HighchartsReact key={index} highcharts={Highcharts} options={chart} />  
+            )}
 
             <Link to='/'>
                 <ButtonComponent variant='outline-dark' size='sm' isArrow={false} textButton='Voltar' className='button-back' ></ButtonComponent>
